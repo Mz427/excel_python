cffcd34864429a03f6e2afa133b1a2078d112e75
Mz427 <53973695+Mz427@users.noreply.github.com>
2021-03-18 15:45:29 +0800

    20210318

---
+++ mytest.py
@@ -0,0 +1,9 @@
+for i in range(0, 5):
+    print(f"====>{i}")
+    i +=1
+    print(i)
+
+a = [1,2,3,4,5]
+
+
+print(a)
--- test.py
+++ test.py
@@ -7,39 +7,45 @@ zb_source_files = ZhouBaoSource()
 current_month_dir = zb_source_files.current_month_dir
 zb_source_files_sheets = zb_source_files.work_week.sheet_name_date()
 zb_bian_jie_zhi = zb_source_files.work_week.bian_jie_zhi
+hui_zong_qs = []
 
-if zb_source_files.work_week.diferent_month:
-    qing_suan(load_workbook(zb_source_files.last_week_qs), zb_source_files_sheets[:zb_bian_jie_zhi])
-    qing_suan(load_workbook(zb_source_files.current_week_qs), zb_source_files_sheets[zb_bian_jie_zhi:])
-else:
-    qing_suan(load_workbook(zb_source_files.current_week_qs, zb_source_files_sheets[:zb_bian_jie_zhi]))
-
-def qing_suan(wb_qs_name, ws_qs_names):
-    hui_zong_qs = []
-    l_ben_di = []
-    l_yi_di = []
+def qing_suan(wb_qs_name, ws_qs_names, hui_zong_qs):
     l_tag = True
     jin_e_column = 10
+    chu_li_column = 36
     wb_qs = load_workbook(wb_qs_name)
 
     for i in ws_qs_names:
         ws_qs = wb_qs[i]
-
-        for j in range(4, ws_qs.max_row + 1):
-            if ws_qs.cell(row = i, column = jin_e_column).value != None and l_tag:
-                if red:
-                    本地未处理笔数 += 1
-                    本地未处理金额 += admount
-                本地总笔数 += 1
-                本地总金额 += admount
-            elif l_tag and ws_week.cell(row = i, column = jin_e_column).value != None:
-                i += 3
+        hui_zong_qs_sheet = [0] * 6
+        j = 4
+        while j <= ws_qs.max_row:
+            if ws_qs.cell(row = j, column = jin_e_column).value != None and l_tag:
+                if ws_qs.cell(row = j, column = chu_li_column).value == "未处理":
+                    hui_zong_qs_sheet[3] += 1
+                    hui_zong_qs_sheet[5] += float(ws_qs.cell(row = j, column = jin_e_column).value)
+                hui_zong_qs_sheet[0] += 1
+                hui_zong_qs_sheet[2] += float(ws_qs.cell(row = j, column = jin_e_column).value)
+            elif ws_qs.cell(row = j, column = jin_e_column).value == None and l_tag:
+                j += 3
+                jin_e_column += 1
+                chu_li_column += 3
                 l_tag = False
-                #continue
             elif not l_tag:
-                l_yi_di.append(ws_week.cell(row = i, column = jin_e_column).value)
+                if ws_qs.cell(row = j, column = jin_e_column).value != None:
+                    if ws_qs.cell(row = j, column = chu_li_column).value == "未处理":
+                        hui_zong_qs_sheet[4] += 1
+                        hui_zong_qs_sheet[5] += float(ws_qs.cell(row = j, column = jin_e_column).value)
+                    hui_zong_qs_sheet[1] += 1
+                    hui_zong_qs_sheet[2] += float(ws_qs.cell(row = j, column = jin_e_column).value)
+            j += 1
 
-            print(l_ben_di)
-            print(l_yi_di)
+        hui_zong_qs.append(hui_zong_qs_sheet)
 
-    return 
+if zb_source_files.work_week.diferent_month:
+    qing_suan(zb_source_files.last_week_qs, zb_source_files_sheets[:zb_bian_jie_zhi], hui_zong_qs)
+    qing_suan(zb_source_files.current_week_qs, zb_source_files_sheets[zb_bian_jie_zhi:], hui_zong_qs)
+else:
+    qing_suan(zb_source_files.current_week_qs, zb_source_files_sheets[:zb_bian_jie_zhi], hui_zong_qs)
+
+print(hui_zong_qs)
